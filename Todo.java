245473aa644a368f0df27c454467ee1ea586df56
Dennis Case <denniscase1999@gmail.com>
2019-02-19 20:12:54 -0600

    Uploading code with Calculator class done

---
+++ Todo.java
@@ -0,0 +1,69 @@
+public class Todo {/*
+Lab Section_________: 13
+Last Modified_______: 2019-02-19
+OU ID_______________: 113467026
+OU EMAIL____________: denniscase1999@ou.edu
+
+Tasks:
+1. Finish calculateTwoTokens method in Calculator class.
+2. Finish calculateThreeTokens method in Calculator class.
+3. Finish execute method in Calculator class.
+4. Finish parseAndExecute method in Calculator class.
+5. Finish calculateTwoTokensTestValidHalve method in CalculatorTest class.
+6. Finish calculateTwoTokensTestInvalidCommand method in CalculatorTest class.
+7. Finish calculateThreeTokensTestValidAdd method in CalculatorTest class.
+8. Finish calculateThreeTokensTestValidSubtract method in CalculatorTest class.
+9. Finish calculateThreeTokensTestValidDivide method in CalculatorTest class.
+10. Finish calculateThreeTokensTestInvalidCommand method in CalculatorTest class.
+11. Finish executeTestValidQuit method in CalculatorTest class.
+12. Finish executeTestValidThreeTokens method in CalculatorTest class.
+13. Finish executeTestInvalidTokenLength method in CalculatorTest class.
+14. Finish parseAndExecuteTestDivideByZero method in CalculatorTest class.
+15. Finish parseAndExecuteTestInvalidNumber method in CalculatorTest class.
+16. Finish parseAndExecuteTestInvalidTokenLength method in CalculatorTest class.
+17. Finish parseAndExecuteTestDivideByZero method in CalculatorTest class.
+18. Finish parseAndExecuteTestDivideByZero method in CalculatorTest class.
+19. Finish parseAndExecuteTestDivideByZero method in CalculatorTest class.
+
+Estimated Time (minutes):
+1. 15
+2. 15
+3. 10
+4. 
+5. 
+6. 
+7. 
+8. 
+9. 
+10. 
+11. 
+12. 
+13. 
+14. 
+15. 
+16. 
+17. 
+18. 
+19. 
+
+Actual Time (minutes):
+1. 20
+2. 20
+3. 10
+4. 
+5. 
+6. 
+7. 
+8.
+9. 
+10. 
+11. 
+12. 
+13. 
+14. 
+15. 
+16. 
+17. 
+18. 
+19. 
+*/}
